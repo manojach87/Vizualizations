0fb8ec4a25c90b1334d904167f7e911543a43049
manojach87 <manoj.ach@gmail.com>
2018-07-28 13:47:00 -0400

    First Real Commit
    First

---
+++ scripts/readSimpleCSV.js
@@ -0,0 +1,21 @@
+  var St="";
+  var idx = [];
+  var itr = [];
+  var data=d3.csv("./us_cities_states_counties1.csv", function(d){ 
+  
+        if(isNaN(itr[St])) {
+        		itr[St]=0
+        		
+        }
+        if(St!=d.St) { 
+        	console.log(d.St);
+        }
+        else {
+        
+        	itr[St]+= 1;
+        }
+        St=d.St;
+        //return d;
+        d3.
+  });
+console.log(itr);
